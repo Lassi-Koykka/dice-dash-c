afc87855781c1b01b8e289589bf827af7a8e73fe
Lassi Köykkä <lassi_koykka@hotmail.com>
2023-11-29 22:55:53 +0200

    Add drawing for background map

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DICE-DASH C-PORT
-A C port of my game [Dice-Dash](https://github.com/lassi-koykka/dice-dash) using [Raylib](https://www.raylib.com/)
+A C port of my game [Dice-Dash](https://github.com/lassi-koykka/dice-dash) 
+using [Raylib](https://www.raylib.com/)
--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include <raylib.h>
 #include <stdio.h>
 
+// Constants
 #define TILE_SIZE 48
+#define POINT_SIZE 8
 #define GRID_W 24
 #define GRID_H 14
 #define SCREEN_W 1280
@@ -12,6 +14,20 @@
 #define H_LANE_END 9
 #define V_LANE_START 10
 #define V_LANE_END 14
+
+// Colors
+// #D3D3D3
+#define DD_LIGHT_GRAY                                                          \
+  (Color) { 0xD3, 0xD3, 0xD3, 0xff }
+// #949494
+#define DD_GRAY                                                                \
+  (Color) { 0x94, 0x94, 0x94, 0xff }
+// #646400
+#define DD_GOLD                                                                \
+  (Color) { 0x64, 0x64, 0x00, 0xff }
+// #141414
+#define DD_DARK_GRAY                                                           \
+  (Color) { 0x14, 0x14, 0x14, 0xff }
 
 int IsInbounds(int row, int col) {
   int onHorizontalLane = row >= H_LANE_START && row < H_LANE_END;
@@ -57,11 +73,30 @@ int main() {
 
     int gridAbsW = TILE_SIZE * GRID_W;
     int gridAbsH = TILE_SIZE * GRID_H;
-
     int startX = (SCREEN_W - gridAbsW) / 2;
     int startY = (SCREEN_H - gridAbsH) / 2;
 
-    DrawRectangle(startX, startY, gridAbsW, gridAbsH, WHITE);
+    for (int row = 0; row < g->h; row++) {
+      for (int col = 0; col < g->w; col++) {
+        int onHorizontalLane = row >= H_LANE_START && row < H_LANE_END;
+        int onVerticalLane = col >= V_LANE_START && col < V_LANE_END;
+
+        int x1 = startX + TILE_SIZE * col;
+        int y1 = startY + TILE_SIZE * row;
+
+        if (onHorizontalLane && onVerticalLane) {
+          DrawRectangle(x1, y1, TILE_SIZE, TILE_SIZE, DD_LIGHT_GRAY);
+          DrawCircle(x1 + TILE_SIZE / 2, y1 + TILE_SIZE / 2, POINT_SIZE,
+                     DD_GRAY);
+        } else if (onHorizontalLane || onVerticalLane) {
+          DrawCircle(x1 + TILE_SIZE / 2, y1 + TILE_SIZE / 2, POINT_SIZE,
+                     DD_GOLD);
+        } else {
+          DrawCircle(x1 + TILE_SIZE / 2, y1 + TILE_SIZE / 2, POINT_SIZE,
+                     DD_DARK_GRAY);
+        }
+      }
+    }
 
     EndDrawing();
     // ----------
